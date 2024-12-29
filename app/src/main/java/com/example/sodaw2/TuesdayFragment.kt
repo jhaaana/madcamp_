47a22556902265f97eaab7024778e1889e319f6f
Leesengmin1003 <cwn36963@kaist.ac.kr>
2024-12-30 00:59:39 +0900

    almost done

--- app/src/main/java/com/example/sodaw2/TuesdayFragment.kt
+++ app/src/main/java/com/example/sodaw2/TuesdayFragment.kt
@@ -18,6 +18,7 @@ class TuesdayFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         val view = inflater.inflate(R.layout.fragment_tuesday, container, false)
         // ViewModel 초기화
